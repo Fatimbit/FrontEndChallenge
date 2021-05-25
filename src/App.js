939e7fb9e9a8be3d5592787667277db71fdb0357
Fatimah Vakily <fvaki076@uottawa.ca>
2021-05-24 20:06:38 -0400

    Added basic validation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 
+import React, {useState} from "react";
+import validator from 'validator';
 import './App.css';
 
 function App() {
+
+
+  const[emailError,setEmailError]=useState('')
+  const validateEmail=(e)=>{
+    var email=e.target.value
+
+    if(validator.isEmail(email)){
+      setEmailError("")}
+      else{
+        setEmailError("This doesn't look like an email, please try again.")
+      }    
+
+  }
+
+  const[passwordError,setPasswordError]=useState('')
+  const validatePassword=(e)=>{
+    var password=e.target.value
+    if(password=""){
+      setPasswordError("Password cannot be empty.")
+    }
+    else if(password<8){
+      setPasswordError("The password must be at least 8 characters.")
+    }
+  }
+
+
+
+
+
+
   return (
     <div className="App">
 
@@ -29,11 +61,13 @@ function App() {
       </p>
 
       <div class="texts">
-      Email <br></br><input type="email" name="email" size="57" ></input><br></br>
-      Password (min. 8 characters) <br></br><input type="password" name="password" size="57"></input><br></br>
+      Email <br></br><input type="text" name="email" size="57" onChange={(e)=> validateEmail(e)}></input><br></br>
+      <span style={{fontWeight:'bold', color:'red',}}>{emailError}</span><br></br>
+      Password (min. 8 characters) <br></br><input type="password" name="password" size="57" onChange={(e)=> validatePassword(e)}></input><br></br>
+      <span style={{fontWeight:'bold', color:'red',}}>{passwordError}</span><br></br>
         </div>
 
-      <input type="checkbox" id="checkbox1" name="checkbox1" value="1"></input>
+      <input type="checkbox" id="checkbox1" name="checkbox1" value="1" ></input>
       <label for="checkbox1" class="texts"> I agree to the <a class="links" href="">Terms of Service</a> and <a class="links" href="">Privacy Policy</a> </label><br></br>
 
       <br></br>
